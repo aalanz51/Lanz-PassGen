abd986a2f1d03783cd97d3c16ea169c0d7ee1044
aalanz51 <austinalexanderlanz@gmail.com>
2021-11-14 23:39:00 -0500

    finalized assignment including picture and all necessary files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Lanz-PassGen
 GT Bootcamp Password Generator assignment
+
+My thought process was that we would need to:
+
+-identify the characters our arrays would use
+
+-create the functions that the user would utilize to designate which password format they would like
+
+-randomize the characters so that the password would be different every time
+
+-present the user with the password they requested
+
--- script.js
+++ script.js
@@ -1,61 +1,64 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-var numbersArray = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 0"];
-var specialArray = ["!, @, #, $, %, ^, &, *, (, )"];
-var uppercaseArray = ["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"];
-var lowercaseArray = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"];
-
-var result = [];
-var userEnter = [];
-
-var onlyNumbers = prompt ("Between 8 and 128, how many characters are required?");
-
-function passwordOptions(onlyNumbers) {
-    if (isNaN(onlyNumbers)) {
-        alert("Enter only numeric values.");
-        return false;
-    } else if (parseInt (useNumCharacters) < 8) {
-        alert("Password must be at least 8 characters.");
-        return false;
-    } else if (parseInt (useNumCharacters) >= 128) {
-        return false;
-        }
-    return true;
-    }
-
-var validPassword === passwordOptions;
-
-    if onlyNumbers = validPassword {
-        var addNumbers = confirm ("Add Numbers?");
-        var addSpecial = confirm ("Add Special Characters?");
-        var addUppercase = confirm ("Add Uppercase Letters?");
-        var addLowercase = confirm ("Add Lowercase Letters?");
-
-        if (addNumbers){
-            resultArray = resultArray.concat(numbersArray); 
-          }
-        if (addSpecial){
-            resultArray = resultArray.concat(specialArray); 
-          }
-        if (addUppercase){
-            resultArray = resultArray.concat(numbersArray); 
-          }
-    }
-
-chosenWord = words[Math.floor(Math.random() * collection.length)];
-lettersInChosenWord = chosenWord.split("");
-numBlanks = lettersInChosenWord.length;
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;}
-
-
-// Add event listener to generate button
+var generateBtn = document.querySelector("#generate")
 generateBtn.addEventListener("click", writePassword);
 
+var numbersArray = ["1234567890"];
+var specialArray = ["!@#$%^&*()"];
+var uppercaseArray = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowercaseArray = ["abcdefghijklmnopqrstuvwxyz"];
 
+var userInput = "";
+var result = "";
+
+function passwordOptions(){
+    var passwordLength = prompt ("Between 8 and 128, how many characters are required?");
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+        alert("Incorrect input. Please enter values in the requested format.");
+        console.log("Incorrect input");
+        return "Try again.";
+    } else {
+        console.log(passwordLength);
+
+    var addNumbers = confirm ("Add Numeric Characters?");
+    if (addNumbers == true) {
+        userInput += numbersArray
+        console.log(addNumbers);
+    }
+
+    var addSpecial = confirm ("Add Special Characters?");
+    if (addSpecial == true) {
+        userInput += specialArray
+        console.log(addSpecial)
+    }
+
+    var addUppercase = confirm ("Add Uppercase Letters?");
+    if (addUppercase == true) {
+        userInput += uppercaseArray
+        console.log(addUppercase);
+    }
+
+    var addLowercase = confirm ("Add Lowercase Letters?");
+    if (addLowercase == true) {
+        userInput += lowercaseArray
+        console.log(addLowercase)
+    }
+    if (addNumbers !== true && addSpecial !== true &&  addUppercase !== true && addLowercase !== true) {
+        alert ("Incorrect input. Please enter values in the requested format.")
+        return
+    } else {
+        console.log(userInput);
+    }
+    for (i = 0; i < passwordLength; ++i) {
+        result += userInput[Math.floor(Math.random() * userInput.length)];
+        console.log(result);
+    }
+        return result
+    }
+}
+// Write password to the #password input
+function writePassword() {
+  var password = passwordOptions();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
